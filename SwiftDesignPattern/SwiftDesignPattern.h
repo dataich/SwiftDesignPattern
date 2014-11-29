249d4424747974cbf2b7584439c0a8f892641016
Taichiro Yoshida <taichiro.yoshida@gmail.com>
2014-11-29 13:56:24 +0900

    #1 prepare project

---
+++ SwiftDesignPattern/SwiftDesignPattern.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDesignPattern.h
+//  SwiftDesignPattern
+//
+//  Created by Taichiro Yoshida on 11/29/14.
+//  Copyright (c) 2014 Taichiro Yoshida. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftDesignPattern.
+FOUNDATION_EXPORT double SwiftDesignPatternVersionNumber;
+
+//! Project version string for SwiftDesignPattern.
+FOUNDATION_EXPORT const unsigned char SwiftDesignPatternVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDesignPattern/PublicHeader.h>
+
+
---
+++ SwiftDesignPatternTests/Source/SwiftDesignPatternTests.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftDesignPatternTests.swift
+//  SwiftDesignPatternTests
+//
+//  Created by Taichiro Yoshida on 11/29/14.
+//  Copyright (c) 2014 Taichiro Yoshida. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SwiftDesignPatternTests: XCTestCase {
+}
